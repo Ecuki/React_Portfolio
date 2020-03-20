1af5e49267e9ea73b2df799a6fdf5f4be67cc371
Emil <jeziorski.emil@gmail.com>
2020-03-21 00:38:32 +0100

    Updates

---
+++ precache-manifest.7f5bfab8cd5325454fb8360a7adbbe7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57550cd6889d20f74093ebac0217493",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "51582e62a04cb50389d8",
+    "url": "/React_Portfolio/static/css/main.7c6ed4f0.chunk.css"
+  },
+  {
+    "revision": "cf26070335e71a455025",
+    "url": "/React_Portfolio/static/js/2.622ba048.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/React_Portfolio/static/js/2.622ba048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51582e62a04cb50389d8",
+    "url": "/React_Portfolio/static/js/main.477b172a.chunk.js"
+  },
+  {
+    "revision": "d663dd32c34690f0bc51",
+    "url": "/React_Portfolio/static/js/runtime-main.895e4125.js"
+  },
+  {
+    "revision": "e70ae992eb236a2ebec4524e8df77579",
+    "url": "/React_Portfolio/static/media/eye.e70ae992.svg"
+  },
+  {
+    "revision": "a9f7ad3dc6d998df5c984f5363913c7f",
+    "url": "/React_Portfolio/static/media/github_icon.a9f7ad3d.svg"
+  }
+]);
